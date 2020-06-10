01e799c4234c20b40f403d148e62b22938a3eece
Lucas Sadzik <ritsuboshi@gmail.com>
2020-06-10 23:58:17 +0200

    feat: Setup database models

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+from models import setup_db, Actor, Movie
+
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
+    setup_db(app)
     CORS(app)
 
     return app
